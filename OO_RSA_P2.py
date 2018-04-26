d15f9a415d87c7e9dbfc2be6fcd331e314288f45
Kris Hunt <sourcekris@users.noreply.github.com>
2018-04-26 22:56:51 +1000

    Add exercises

--- OO_RSA_P2.py
+++ OO_RSA_P2.py
@@ -8,26 +8,11 @@
 import random
 import libnum
 
-# isPrime tests if x is prime or not by trial division.
-# TODO(flypig2016): Find a more efficient method!
-def isPrime(x):
-  for i in xrange(2, x-1):
-    if x % i == 0:
-      return False
-  return True
-
-# generatPrime returns a prime number of bit length bits.
-def generatePrime(bits):
-  while True:
-    value = random.getrandbits(bits)
-    if isPrime(value):
-      return value
-
 # RSAKeys inherits from the object base class and stores state and holds methods for constructing
 # and printing an RSA Key Pair.
 class RSAKeys(object):
   # type is a member that belongs to ALL instances of objects of type RSAPublicKey.
-  keytype = "public/private key pair"
+  type = "public/private key pair"
   # e is common to all key pairs I care about.
   e = 65537
 
@@ -35,8 +20,8 @@ class RSAKeys(object):
   def __init__(self, bitlength):
     # construct a new key pair here in this constructor.
     while True:
-      self.p = generatePrime(bitlength)
-      self.q = generatePrime(bitlength)
+      self.p = libnum.generater_prime(bitlength)
+      self.q = libnum.generate_prime(bitlength)
 
       # p and q must be co-prime.
       if libnum.gcd(self.p,self.q) == 1:
@@ -76,7 +61,7 @@ if __name__ == "__main__":
 
   # 1. Write the methods for returning just the public / private keys 
   # 2. Write some stuff in the main function to print some of the members of an RSAKey object
-  # 3. Construct a few more keys. Compare the value of e across them. Compare the value of the keytype member across them
+  # 3. Construct a few more keys. Compare the value of e across them. Compare the value of the type member across them
   # 4. Write a class just for PublicKeys and another one just for Private Keys. Have RSAKey instantiate an object of each.
 
 
