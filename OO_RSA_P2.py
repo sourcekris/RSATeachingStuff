c112313027288b592ae431169a711b8903fec538
Kris Hunt <sourcekris@users.noreply.github.com>
2018-04-26 23:00:59 +1000

    Add more to P2

--- OO_RSA_P2.py
+++ OO_RSA_P2.py
@@ -1,18 +1,50 @@
 #!/usr/bin/env python2
 
-# OO_RSA_P1.py is the first part of the object oriented Python lesson. We want to demonstrate 
-# what a class / object are in python, how they store state and have methods. How they are 
-# constructed and how they are defined. The difference between members that are shared and members
-# that are private etc.
+# OO_RSA_P2.py is the second part of the object oriented Python lesson.
 
 import random
 import libnum
 
+class RSAPrivateKey(object):
+  keytype = "private"
+
+  def __init__(self):
+    while True:
+      self.p = libnum.generater_prime(bitlength)
+      self.q = libnum.generate_prime(bitlength)
+
+      # p and q must be co-prime.
+      if libnum.gcd(self.p,self.q) == 1:
+        break
+
+    self.phi = (self.p - 1) * (self.q - 1)
+    self.d = libnum.invmod(self.e, self.phi)
+    self.n = self.p * self.q
+
+  def String(self):
+    return "RSAPrivateKey Object: {{{0},{1}}}".format(self.n, self.d)
+
+  def Show(self):
+    print self.String()
+  
+class RSAPublicKey(object):
+  keytype = "public"
+
+  def __init__(self, privateKey):
+    self.n = privateKey.n
+    self.e = privateKey.e
+
+  def String(self):
+    return "RSAPublicKey Object: {{{0},{1}}}".format(self.n, self.e)
+
+  def Show(self):
+    print self.String()
+
 # RSAKeys inherits from the object base class and stores state and holds methods for constructing
 # and printing an RSA Key Pair.
 class RSAKeys(object):
-  # type is a member that belongs to ALL instances of objects of type RSAPublicKey.
-  type = "public/private key pair"
+  # keytype is a member that belongs to ALL instances of objects of type RSAPublicKey.
+  keytype = "public/private key pair"
   # e is common to all key pairs I care about.
   e = 65537
 
@@ -45,13 +77,11 @@ class RSAKeys(object):
 
   # PublicKey returns a tuple of the PublicKey components.
   def PublicKey(self):
-    # TODO(flypig2016): implement me
-    pass
+    return (self.n, self.e)
 
   # PrivateKey returns a tuple of the PrivateKey components.
   def PrivateKey(self):
-    # TODO(flypig2016): implement me
-    pass
+    return (self.n, self.d)
 
 if __name__ == "__main__":
   b = RSAKeys(10)
@@ -59,9 +89,6 @@ if __name__ == "__main__":
 
   # excercises ?
 
-  # 1. Write the methods for returning just the public / private keys 
-  # 2. Write some stuff in the main function to print some of the members of an RSAKey object
-  # 3. Construct a few more keys. Compare the value of e across them. Compare the value of the type member across them
   # 4. Write a class just for PublicKeys and another one just for Private Keys. Have RSAKey instantiate an object of each.
 
 
